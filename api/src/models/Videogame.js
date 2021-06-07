d04c921ed8142b287bac8a600922461be5af458b
rafamossetto <mossettorafael@gmail.com>
2021-06-07 20:55:00 -0300

    backend

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,28 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    releaseDate: {
+      type: DataTypes.STRING
+    },
+    rating: {
+      type: DataTypes.INTEGER
+    },
+    platforms: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,55 @@
 const { Router } = require('express');
+const router = Router();
+const axios = require('axios').default;
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-
-const router = Router();
+// GET a '/videogames
+router.get('/videogames', (req, res) => {
+    if (req.query.name) {
+        return axios.get(`https://api.rawg.io/api/games?search=${req.query.name}&key=c542e67aec3a4340908f9de9e86038af`)
+            .then(response => {
+                if (!response.data.count) return res.send(`No se encontro ningun videojuego con el nombre "${req.query.name}"`);
+                return res.json(response.data.results.splice(0, 15))
+            })
+            .catch(err => res.status(500).json(err))
+    } else {
+        axios.get('https://api.rawg.io/api/games?key=c542e67aec3a4340908f9de9e86038af')
+            .then(response => res.json(response.data.results.splice(0, 15)))
+            .catch(err => res.status(500).json(err))
+    }
+})
+// GET /videogame/:idVideoGame
+router.get('/videogame/:idVideogame', (req, res) => {
+    const { idVideogame } = req.params
+    const id = parseInt(idVideogame);
+    axios.get(`https://api.rawg.io/api/games/${id}?key=c542e67aec3a4340908f9de9e86038af`)
+        .then(response => {
+            const { name, background_image, genres, description, released, rating, platforms } = response.data;
+            res.json({
+                name,
+                background_image,
+                genres,
+                description,
+                released,
+                rating,
+                platforms
+            })
+        })
+        .catch(err => res.status(500).json(err))
+})
+// GET a /genres
+router.get('/genres', async (req, res) => {
+    const response = await axios.get('https://api.rawg.io/api/genres?key=c542e67aec3a4340908f9de9e86038af');
+    const genres = response.data.results;
+    res.json(genres)
+    //Terminar
+}) 
+//POST a /videogame
+router.post('/videogame', (req, res) => {
+    // const { game } = req.body
+    res.json(req.body)
+})
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
