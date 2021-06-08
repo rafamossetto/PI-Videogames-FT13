2c84130f5cdb2ae5aaae3f7f7f12d6ab7127fd90
rafamossetto <mossettorafael@gmail.com>
2021-06-07 21:46:56 -0300

    backend first v

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,20 +1,23 @@
+require('dotenv').config();
+const { APIKEY } = process.env;
 const { Router } = require('express');
 const router = Router();
 const axios = require('axios').default;
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 // GET a '/videogames
 router.get('/videogames', (req, res) => {
     if (req.query.name) {
-        return axios.get(`https://api.rawg.io/api/games?search=${req.query.name}&key=c542e67aec3a4340908f9de9e86038af`)
+        return axios.get(`https://api.rawg.io/api/games?search=${req.query.name}&key=${APIKEY}`)
             .then(response => {
                 if (!response.data.count) return res.send(`No se encontro ningun videojuego con el nombre "${req.query.name}"`);
                 return res.json(response.data.results.splice(0, 15))
             })
             .catch(err => res.status(500).json(err))
     } else {
-        axios.get('https://api.rawg.io/api/games?key=c542e67aec3a4340908f9de9e86038af')
+        axios.get(`https://api.rawg.io/api/games?key=${APIKEY}`)
             .then(response => res.json(response.data.results.splice(0, 15)))
             .catch(err => res.status(500).json(err))
     }
@@ -23,7 +26,7 @@ router.get('/videogames', (req, res) => {
 router.get('/videogame/:idVideogame', (req, res) => {
     const { idVideogame } = req.params
     const id = parseInt(idVideogame);
-    axios.get(`https://api.rawg.io/api/games/${id}?key=c542e67aec3a4340908f9de9e86038af`)
+    axios.get(`https://api.rawg.io/api/games/${id}?key=${APIKEY}`)
         .then(response => {
             const { name, background_image, genres, description, released, rating, platforms } = response.data;
             res.json({
@@ -40,11 +43,11 @@ router.get('/videogame/:idVideogame', (req, res) => {
 })
 // GET a /genres
 router.get('/genres', async (req, res) => {
-    const response = await axios.get('https://api.rawg.io/api/genres?key=c542e67aec3a4340908f9de9e86038af');
+    const response = await axios.get(`https://api.rawg.io/api/genres?key=${APIKEY}`);
     const genres = response.data.results;
     res.json(genres)
     //Terminar
-}) 
+})
 //POST a /videogame
 router.post('/videogame', (req, res) => {
     // const { game } = req.body
